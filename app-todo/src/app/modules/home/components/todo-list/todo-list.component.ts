130ae21a9346da83c641e744d01d754eda8436e6
Rodrigo Weber <rodrigoweber97@hotmail.com>
2022-09-25 10:55:37 -0300

    remove buttons created

--- app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
+++ app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -8,11 +8,22 @@ import { TaskList } from '../../model/task-list';
 })
 export class TodoListComponent implements OnInit {
   public taskList: Array<TaskList> = [
-    {task: 'My new task 1', checked: true},
-    {task: 'My new task 2', checked: false},
-    {task: 'My new task 3', checked: false},
+    { task: 'My new task 1', checked: true },
+    { task: 'My new task 2', checked: false },
+    { task: 'My new task 3', checked: false },
   ];
   constructor() {}
 
   ngOnInit(): void {}
+
+  public deleteItemTaskList(event: number) {
+    this.taskList.splice(event, 1);
+  }
+
+  public deleteAllTaskList() {
+    const confirm = window.confirm('Você deseja deletar toda a lista?');
+    if (confirm) {
+      this.taskList = [];
+    }
+  }
 }

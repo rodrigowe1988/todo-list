0bf4bfbf4029ef2e9e3d28be1111f3463372b982
Rodrigo Weber <rodrigoweber97@hotmail.com>
2022-09-25 09:19:03 -0300

    show and hide the logo and delete all button

--- app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
+++ app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
+  public taskList: Array<TaskList> = [
+    {task: 'My new task 1', checked: true},
+    {task: 'My new task 2', checked: false},
+    {task: 'My new task 3', checked: false},
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- app-todo/src/app/modules/home/home.module.ts
+++ app-todo/src/app/modules/home/home.module.ts
@@ -5,6 +5,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { TodoInputAddItensComponent } from './components/todo-input-add-itens/todo-input-add-itens.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { HomeComponent } from './pages/home/home.component';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -17,7 +18,8 @@ import { HomeComponent } from './pages/home/home.component';
     HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class HomeModule { }
